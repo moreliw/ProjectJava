db20d2dfddf4b458ff5ef644743d4232922d1093
William <williammorelli07@gmail.com>
2022-04-07 01:19:26 -0300

    Initial commit

---
+++ src/Classes/Calculator.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Classes;
+
+/**
+ *
+ * @author William Moreli
+ */
+public class Calculator {
+    private int numeroA;
+    private int numeroB;
+    
+    public int getNumeroA() {
+        return numeroA;
+    }
+
+    public void setNumeroA(int numeroA) {
+        this.numeroA = numeroA;
+    }
+
+    public int getNumeroB() {
+        return numeroB;
+    }
+
+    public void setNumeroB(int numeroB) {
+        this.numeroB = numeroB;
+    }
+
+    public int SomarNumeros(int numA, int numB){
+        return (numA + numB);
+    }
+}
